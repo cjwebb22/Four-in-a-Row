f7480cda9ea0758754e633dd4d49f06b53cc984a
cjwebb22 <40739709+cjwebb22@users.noreply.github.com>
2022-10-08 03:29:55 +1100

    Add files via upload

---
+++ AIPlayer.py
@@ -0,0 +1,53 @@
+from Board import Board
+from copy import deepcopy
+from random import choice
+
+def AIChooseColumn(board):
+    board = deepcopy(board)
+    choices = None
+    depth = 5
+    for i in range(board.columns):
+        if board.board[-1][i] == "0":
+            choices = [i + 1]
+            score = minimax(depth, board, "2", False, choices[0])
+            break
+    for i in range(choices[0] + 1, board.columns + 1):
+        temp = minimax(depth, board, "2", False, i)
+        if temp > score:
+            score = temp
+            choices = [i]
+        elif score == temp:
+            choices.append(i)
+    if choices != None:
+        return choice(choices)
+
+def minimax(depth: int, board: Board, player: str, maximiser: bool, column: int) -> int:
+    board = deepcopy(board)
+    if not board.put_in_column(player, column):
+        return
+    if depth <= 0:
+        return 0
+    if board.check_winner("1"):
+        return -depth
+    elif board.check_winner("2"):
+        return depth
+    elif board.is_full():
+        return 0
+    elif not maximiser:
+        best = -depth + 1
+        score = 1000
+        for i in range(1, board.columns+1):
+            if board.board[-1][i-1] == "0":
+                score = min(score, minimax(depth-1, board, str(3-int(player)), True, i))
+                if score == best:
+                    break
+    else:
+        best = depth-1
+        score = -1000
+        for i in range(1, board.columns+1):
+            if board.board[-1][i-1] == "0":
+                score = max(score, minimax(depth-1, board, str(3-int(player)), False, i))
+                if score == best:
+                    break
+    return score
+
---
+++ Game(3).py
@@ -0,0 +1,146 @@
+from Board import Board
+from AIPlayer import AIChooseColumn
+    
+class Game:
+    """
+    This class defines the game.
+    """
+    def __init__(self):
+        """
+        Initialises game
+        """
+        self.board = Board()
+        self.outcome = None
+        self.menu()
+    
+    def execute_turn(self, player):
+        """
+        Executes a turn.
+
+        Arguments:
+        - player : a string representing player playing
+
+        Returns a boolean representing whether the game has ended.
+        """
+        #get input
+        while True:
+            column = input(f"Player {player} please input a column number or '.' to go back to the menu: ")
+            #check if input valid
+            try:
+                column = int(column)
+                if self.board.put_in_column(player, column):
+                    break
+                else:
+                    print(f"Column full. Please input a column that isn't full or '.' to go back to the menu.")
+            except ValueError:
+                if column == ".":
+                    self.outcome = "HALT"
+                    break
+                else:
+                    print(f"Please input an integer or '.' to go back to the menu.")
+        #check if halting
+        if column == ".":
+            return True
+        print(self.board)
+        #check if winner
+        if self.board.check_winner(player):
+            self.outcome = f"Player {player} wins!"
+            return True
+        #check if draw
+        elif self.board.is_full():
+            self.outcome = "Draw"
+            return True
+        return False
+    
+    def execute_AI_turn(self) -> bool:
+        """
+        Executes an AI player turn. The AI player is always player 2.
+
+        Returns a bool representing whether the game has ended.
+        """
+        #get input
+        choice = AIChooseColumn(self.board)
+        self.board.put_in_column("2", choice)
+        print(f"Player 2 put a token in column {choice}")
+        print(self.board)
+        #check if winner
+        if self.board.check_winner("2"):
+            self.outcome = f"Player 2 wins!"
+            return True
+        #chec if draw
+        elif self.board.is_full():
+            self.outcome = "Draw"
+            return True
+        return False
+    def multiplayer_game(self):
+        """
+        Runs a 2 player game of connect 4.
+        """
+        print(self.board)
+        #execute turns
+        while True:
+            if self.execute_turn("1"):
+                break
+            if self.execute_turn("2"):
+                break
+        print(self.outcome)
+        self.board = Board()
+    
+    def solo_game(self):
+        """
+        Runs a solo game with from AIPlayer.
+        """
+        print(self.board)
+        #execute turns
+        while True:
+            if self.execute_turn("1"):
+                break
+            if self.execute_AI_turn():
+                break
+        print(self.outcome)
+        self.board = Board()
+    def print_rules(self):
+        """
+        Prints the rules of connect 4.
+        """
+        #print out rules
+        print("Connect 4 has pretty simple rules.")
+        print("Each player enters a column number 1-7 to place a token in that column.")
+        print("The first player with 4 tokens in a row (horizontally, vertically or diagonally) wins!")
+        print("If the board is full and no player has won it's a draw.")
+        input("Please press enter to go back.")
+    
+    def menu(self):
+        """
+        Menu for game.
+        """
+        while True:
+            #print out game options
+            print("Welcome to Connect-Four!")
+            print("1. Play multiplayercgame")
+            print("2. Play solo game")
+            print("3. Display rules")
+            print("4. Exit")
+            #get input
+            while True:
+                try:
+                    option = int(input("Please enter an option: "))
+                    if option in range(1,5):
+                        break
+                except ValueError:
+                    pass
+                print("Invalid input.")
+            #act on input
+            if option == 1:
+                self.multiplayer_game()
+            elif option == 2:
+                self.solo_game()
+            elif option == 3:
+                self.print_rules()
+            else:
+                print("Goodbye!")
+                break
+            
+
+
+
