08b38e43950f0de6f2de5f50ba04ede06fe3c4de
cjwebb22 <40739709+cjwebb22@users.noreply.github.com>
2022-10-08 05:53:01 +1100

    Update AIPlayer.py

--- AIPlayer.py
+++ AIPlayer.py
@@ -5,26 +5,31 @@ from random import choice
 def AIChooseColumn(depth: int, board: Board) -> str:
     board = deepcopy(board)
     choices = None
+    #get first choice to compare to
     for i in range(board.columns):
-        if board.board[-1][i] == "0":
+        if board.board[-1][i] == "0": #if column isn't full
             choices = [i + 1]
             score = minimax(depth, board, "2", False, choices[0])
             break
+    #go through other columns
     for i in range(choices[0] + 1, board.columns + 1):
-        temp = minimax(depth, board, "2", False, i)
-        if temp > score:
-            score = temp
-            choices = [i]
-        elif score == temp:
-            choices.append(i)
+        if board.board[-1][i-1] == "0": #if column isn't full 
+            temp = minimax(depth, board, "2", False, i)
+            if temp > score:
+                score = temp
+                choices = [i]
+            elif score == temp:
+                choices.append(i)
     if choices != None:
         return choice(choices)
 
 def minimax(depth: int, board: Board, player: str, maximiser: bool, column: int) -> int:
     board = deepcopy(board)
+    #put token in column and check if it can be put in column
     if not board.put_in_column(player, column):
         return
-    if depth <= 0:
+    #return heuristic
+    elif depth <= 0:
         return 0
     elif board.check_winner("1"):
         return -depth
@@ -32,6 +37,7 @@ def minimax(depth: int, board: Board, player: str, maximiser: bool, column: int)
         return depth
     elif board.is_full():
         return 0
+    #return minimum score if not maximiser
     elif not maximiser:
         best = -depth + 1
         score = 1000
@@ -40,6 +46,7 @@ def minimax(depth: int, board: Board, player: str, maximiser: bool, column: int)
                 score = min(score, minimax(depth-1, board, str(3-int(player)), True, i))
                 if score == best:
                     break
+    #return maximum score if maximiser
     else:
         best = depth-1
         score = -1000
